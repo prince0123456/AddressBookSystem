69dacfc094fef139c530d26e6f326f13945614ab
Prince Gupta <princegupta.pg46@gmail.com>
2023-06-07 20:59:36 +0530

    UC13

---
+++ FileReadWrite.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookOOPs
+{
+    public class FileReadWrite
+    {
+        public static string textFilePath = @"F:\BL_.net278\Demo\AddressBookSystem\AddressBook.txt";
+
+        /// <summary>
+        /// writing the contacts to a text file using StreamWriter 
+        /// <summary>
+        /// <param name="contactList"></param>;
+        public static void WriteIntoTextFile(List<Contact> contactList)
+        {
+            if (File.Exists(textFilePath))
+            {
+                using (StreamWriter sw = File.AppendText(textFilePath))
+                {
+                    foreach (Contact contact in contactList)
+                    {
+                        sw.WriteLine(contact);
+                    }
+                    sw.Close();
+                }
+                Console.WriteLine("Successfully written into text file");
+            }
+        }
+
+        /// <summary>
+        /// Reading contactsfrom text file using StreamWriter
+        /// </summary>
+        public static void ReadFromTextFile()
+        {
+            if (File.Exists(textFilePath))
+            {
+
+                using (StreamReader sr = File.OpenText(textFilePath))
+                {
+                    string text = "";
+                    while ((text = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(text);
+                    }
+                    sr.Close();
+                }
+            }
+        }
+    }
+}
